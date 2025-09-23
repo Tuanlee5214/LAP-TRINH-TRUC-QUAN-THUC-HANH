698e87bb3e9b54c826c4f4d85520780ce4249631
Lê Anh Tuấn <23521711@gm.uit.edu.vn>
2025-09-23 19:16:57 +0700

    Add BaiTap2.cs in Tuan 1

---
+++ Tuần 1/BaiTap2/BaiTap2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BaiTap2
+{
+    internal class Program
+    {
+        static bool KtraSoNguyenTo(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Nhap so nguyen duong n: ");
+            n = int.Parse(Console.ReadLine());
+            int tong = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (KtraSoNguyenTo(i))
+                    tong += i;
+            }
+            Console.WriteLine("Tong cac so nguyen to nho hon n la : " + tong);
+        }
+    }
+}
